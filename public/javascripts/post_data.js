bf986048555d6e3627d2858e210a33da4d92ecbf
miwa <>
2018-01-02 02:16:09 +0900

    内部リロード

--- public/javascripts/post_data.js
+++ public/javascripts/post_data.js
@@ -1,9 +1,14 @@
-function doReloadWithCache() {
-    // キャッシュを利用してリロード
-    window.location.reload(false);
-}
-
-window.addEventListener('load', function () {
-    // ページ表示完了した0.5秒後にリロード
-    setTimeout(doReloadWithCache, 500);
+// function doReloadWithCache() {
+//     // キャッシュを利用してリロード
+//     window.location.reload(false);
+// }
+//
+// window.addEventListener('load', function () {
+//     // ページ表示完了した0.5秒後にリロード
+//     setTimeout(doReloadWithCache, 500);
+// });
+$(function(){
+$('#tsts_bt').click(function(){
+  window.location.reload(false);
+})
 });
