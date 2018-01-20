f8466cd309a848cbb48381f0216c0539b47311a6
miwa_shuntaro <>
2018-01-21 06:44:22 +0900

    top・rank(仮)で完成

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,13 @@ $(function(){
   //inview2
   $('.under_line_1').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
 		if(isInView){
-			$(this).stop().addClass('under_line_2');
+			$(this).stop().addClass('under_line_view');
+		}
+	});
+  //inview3
+  $('.under_line_2').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).stop().addClass('under_line_view');
 		}
 	});
 });
