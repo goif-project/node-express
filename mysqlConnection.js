afe39ed8f5afa9bbbcdf0a6491a4c84db37c1bcf
miwa_shuntaro <>
2018-01-21 17:01:27 +0900

    外部db追加

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+// var dbConfig = {
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: 'jack50611',
+//   database: 'golf_db'
+// };
+
 var dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'jack50611',
-  database: 'golf_db'
+ host: 'mysql328.db.sakura.ne.jp',
+ user: 'mayomayooo',
+ password: 'mayomayo25',
+ database: 'mayomayooo_golf-score'
 };
 
 var connection = mysql.createConnection(dbConfig);
