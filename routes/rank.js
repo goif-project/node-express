1002c6ef789e3c467cde5d505bc22f0665720c97
miwa_shuntaro <>
2018-01-24 03:12:51 +0900

    jsonに変更(sql文は要変更、upはしてない)

--- routes/rank.js
+++ routes/rank.js
@@ -1,32 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
-var connection = require('../mysqlConnection');
-//1~10位までの順位を取得するsql文
-var rank1_query = 'SELECT * from total_score ORDER BY score desc LIMIT 3';
-//11~50位までの順位を取得するsql文
-var rank2_query = 'SELECT * from total_score ORDER BY score desc LIMIT 3,17';
-//100位までの順位を取得するsql文
-var rank3_query = 'SELECT * from total_score ORDER BY score desc LIMIT 49,1';
-//200位までの順位を取得するsql文
-var rank4_query = 'SELECT * from total_score ORDER BY score desc LIMIT 99,1';
-
 
 router.get('/', function(req, res, next) {
-  connection.query(rank1_query, function(err, rows1) {
-    connection.query(rank2_query, function(err, rows2) {
-      connection.query(rank3_query, function(err, rows3) {
-        connection.query(rank4_query, function(err, rows4) {
-          res.render('rank', {
-            title: 'HOME GOLF -RANK-',
-            Rank1: rows1,
-            Rank2: rows2,
-            Rank3: rows3,
-            Rank4: rows4
-          });
-        });
-      });
-    });
+  res.render('rank', {
+    title: 'HOME GOLF -RANK-'
   });
 });
 
